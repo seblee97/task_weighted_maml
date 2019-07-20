b9baee718013cfc4911f43d9192932c6bcc4c901
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2019-07-21 01:22:40 +0200

    changed model code to copy parameters from outer model more explicitly using .clone(). This shohould now work although a simpler engineering solution must exist..

--- maml.py
+++ maml.py
@@ -16,17 +16,33 @@ from torch import nn
 class ModelNetwork(nn.Module):
     
     def __init__(self, params):
-        self.params = params
         nn.Module.__init__(self)
-        self.construct_layers()
+
+        self.params = params
+        self.device = self.params.get("device")
+
+        self.weights = []
+        self.biases = []
+
+        self.layer_dimensions = [self.params.get("input_dimension")] \
+                                + self.params.get("network_layers") \
+                                + [self.params.get("output_dimension")]
+
+        self._construct_layers()
 
     @abstractmethod
-    def construct_layers(self):
+    def _construct_layers(self):
         raise NotImplementedError("Base class abstract method")
 
     @abstractmethod
     def forward(self, x):
         raise NotImplementedError("Base class abstract method")
+
+    def _reset_parameters(self):
+        for l in range(len(self.layer_dimensions) - 1):
+            std = 1. / np.sqrt(self.layer_dimensions[l])
+            self.weights[l].data.uniform_(-std, std) # uniform Gaussian initialisation
+            self.biases[l].data.uniform_(-std, std)
 
 
 class MAML(ABC):
@@ -44,8 +60,12 @@ class MAML(ABC):
         self.validation_frequency = self.params.get("validation_frequency")
         self.checkpoint_path = self.params.get("checkpoint_path")
         self.validation_task_batch_size = self.params.get("validation_task_batch_size")
+        
+        self.model_outer = copy.deepcopy(self.model_inner).to(self.device)
 
-        self.meta_optimiser = optim.Adam(self.model.parameters(), lr=self.meta_lr)
+        self.meta_optimiser = optim.Adam(
+            self.model_outer.weights + self.model_outer.biases, lr=self.meta_lr
+            )
 
         # write copy of config_yaml in model_checkpoint_folder
         self.params.save_configuration(self.checkpoint_path)
@@ -56,10 +76,6 @@ class MAML(ABC):
 
     @abstractmethod
     def _generate_batch(self, task, batch_size=25):
-        """
-        generates an array, x_batch, of B datapoints sampled randomly between domain_bounds
-        and computes the sin of each point in x_batch to produce y_batch.
-        """
         raise NotImplementedError("Base class abstract method")
 
     @abstractmethod
@@ -67,40 +83,68 @@ class MAML(ABC):
         raise NotImplementedError("Base class abstract method")
 
     def outer_training_loop(self):
+
+        # get copies of meta network parameters
+        weight_copies = [w.clone() for w in self.model_outer.weights]
+        bias_copies = [b.clone() for b in self.model_outer.biases]
+
+        # initialise cumulative gradient to be used in meta update step
         meta_update_gradients = []
+
         for _ in range(self.task_batch_size):
-            meta_update_gradient = self.inner_training_loop()
+            meta_update_gradient = self.inner_training_loop(weight_copies, bias_copies)    
             meta_update_gradients.append(meta_update_gradient)
 
         # meta update
         for meta_update_gradient in meta_update_gradients:
-            model_state_dict = self.model.state_dict()
-            for parameter_name, parameter in model_state_dict.items():
-                transformed_parameter = parameter - self.meta_lr * torch.abs(meta_update_gradient[parameter_name])
-                model_state_dict[parameter_name].copy_(transformed_parameter)
+            
+            # zero previously collected gradients
+            self.meta_optimiser.zero_grad()
+            outer_model_state_dict = self.model_outer.state_dict()
+            for parameter_name, parameter in outer_model_state_dict.items():
+                transformed_parameter = parameter - self.meta_lr * meta_update_gradient[parameter_name] / self.task_batch_size # TODO: change variable name to tasks_per_meta_update
+                outer_model_state_dict[parameter_name].copy_(transformed_parameter)
 
-    def inner_training_loop(self):
-        model_copy = copy.deepcopy(self.model)
-        inner_optimiser = optim.Adam(model_copy.parameters(), lr=self.inner_update_lr)
+    def inner_training_loop(self, weight_copies, bias_copies):
+
+        # reset network weights to meta network weights
+        self.model_inner.weights = [w.clone() for w in weight_copies]
+        self.model_inner.biases = [b.clone() for b in bias_copies]
+
+        # sample a task from task distribution and generate x, y tensors for that task
         task = self._sample_task()
         x_batch, y_batch = self._generate_batch(task=task, batch_size=self.inner_update_batch_size)
-        for _ in range(self.num_inner_updates):
-            prediction = model_copy.forward(x_batch)
-            loss = self._compute_loss(prediction, y_batch)
-            inner_optimiser.zero_grad()
-            loss.backward()
-            inner_optimiser.step()
 
+        for _ in range(self.num_inner_updates):
+
+            # forward pass
+            prediction = self.model_inner(x_batch)
+
+            # compute loss
+            loss = self._compute_loss(prediction, y_batch)
+
+            # compute gradients wrt inner model copy
+            gradients = torch.autograd.grad(loss, self.model_inner.weights + self.model_inner.biases, create_graph=True, retain_graph=True)
+
+            # update inner model using current model # TODO: HAVE YOU BUILT UP COMPUTATION GRAPH?
+            for i in range(len(self.model_inner.weights)):
+                self.model_inner.weights[i] = self.model_inner.weights[i] - self.inner_update_lr * gradients[i]
+            for j in range(len(self.model_inner.biases)):
+                self.model_inner.biases[j] = self.model_inner.biases[j] - self.inner_update_lr * gradients[i + j + 1] 
+
+        # generate x, y tensors for meta update task sample
         meta_update_samples_x, meta_update_samples_y = self._generate_batch(task=task, batch_size=self.inner_update_batch_size)
-        meta_update_prediction = model_copy.forward(meta_update_samples_x)
+
+        # forward pass for meta update
+        meta_update_prediction = self.model_inner(meta_update_samples_x)
+
+        # compute loss
         meta_update_loss = self._compute_loss(meta_update_prediction, meta_update_samples_y)
-        meta_update_grad = torch.autograd.grad(meta_update_loss, self.model.parameters(), allow_unused=True)
-        import pdb; pdb.set_trace()
-        meta_update_loss.backward()
-        meta_update_gradients = {param_name: param.grad for param_name, param in model_copy.named_parameters()}
-        print("----inner_loss", meta_update_loss)
-        print("----inner network copy params", list(model_copy.parameters()))
-        return meta_update_gradients
+
+        # compute gradients wrt outer model (meta network)
+        meta_update_grad = torch.autograd.grad(meta_update_loss, self.model_outer.weights + self.model_outer.biases)
+
+        return meta_update_grad
 
     def train(self):
         for training_loop in range(self.training_iterations):
@@ -115,25 +159,25 @@ class MAML(ABC):
     def validate(self, visualise=False):
         overall_validation_loss = 0
         for _ in range(self.validation_task_batch_size):
-            validation_network = copy.deepcopy(self.model)
-            validation_optimiser = optim.Adam(validation_network.parameters(), lr=self.inner_update_lr)
+            validation_network = copy.deepcopy(self.model_outer)
+            validation_optimiser = optim.Adam(validation_network.weights + validation_network.biases, lr=self.inner_update_lr)
             validation_task = self._sample_task()
             validation_x_batch, validation_y_batch = self._generate_batch(task=validation_task, batch_size=self.inner_update_batch_size)
             for _ in range(self.num_inner_updates):
-                import pdb; pdb.set_trace()
-                validation_prediction = validation_network.forward(validation_x_batch)
+                validation_prediction = validation_network(validation_x_batch)
                 validation_loss = self._compute_loss(validation_prediction, validation_y_batch)
-                validation_optimiser.zero_grad()
-                validation_loss.backward()
-                validation_optimiser.step()
-            final_validation_prediction = validation_network.forward(validation_x_batch)
+                validation_update_grad = torch.autograd.grad(validation_loss, validation_network.weights + validation_network.biases)
+                for i in range(len(validation_network.weights)):
+                    validation_network.weights[i] = validation_network.weights[i] - self.inner_update_lr * validation_update_grad[i]
+                for j in range(len(validation_network.biases)):
+                    validation_network.biases[j] = validation_network.biases[j] - self.inner_update_lr * validation_update_grad[i + j + 1]
+            final_validation_prediction = validation_network(validation_x_batch)
             final_validation_loss = self._compute_loss(final_validation_prediction, validation_y_batch)
             # test_task = self._sample_task()
             # test_x_batch, test_y_batch = self._generate_batch(task=validation_task, batch_size=self.inner_update_batch_size)
             # test_prediction = validation_network(test_x_batch)
             # test_loss = self._compute_loss(test_prediction, test_y_batch)
             overall_validation_loss += float(final_validation_loss)
-        import pdb; pdb.set_trace()
         print('--- validation loss', overall_validation_loss / self.validation_task_batch_size)
         if visualise:
             self.visualise()
@@ -142,7 +186,7 @@ class MAML(ABC):
         os.makedirs(self.checkpoint_path, exist_ok=True)
         timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%H-%M-%S')
         PATH = '{}model_checkpoint_{}.pt'.format(self.checkpoint_path, timestamp)
-        torch.save(self.model.state_dict(), PATH)
+        torch.save(self.model_outer.state_dict(), PATH)
 
     @abstractmethod
     def visualise(self):
