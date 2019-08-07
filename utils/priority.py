7c607f4870c4fd5be8abe8e6cc2bcf45b862fa93
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2019-08-07 16:09:33 -0700

    changed queue data type to multi-dimensional numpy array, added visualisation of priority queue

--- utils/priority.py
+++ utils/priority.py
@@ -1,14 +1,24 @@
 import operator
 import random
+import numpy as np
+import matplotlib.pyplot as plt
 
-from typing import List, Dict
+from typing import List, Dict, Tuple
 
 class PriorityQueue(object):
 
-    def __init__(self, block_sizes: Dict[str, float], param_ranges: Dict[str, Tuple[float, float]], sample_type: str, burn_in: int=None): 
+    def __init__(self, 
+                block_sizes: Dict[str, float], param_ranges: Dict[str, Tuple[float, float]], 
+                sample_type: str, epsilon_start: float, epsilon_final: float, epsilon_decay_rate: float,
+                initial_value: float=None, burn_in: int=None
+                ): 
         self.block_sizes = block_sizes
         self.param_ranges = param_ranges
         self.sample_type = sample_type
+        self.initial_value = initial_value
+        self.epsilon = epsilon_start
+        self.epsilon_final = epsilon_final
+        self.epsilon_decay_rate = epsilon_decay_rate 
         self.burn_in = burn_in
 
         self.queue = self._initialise_queue()
@@ -17,16 +27,32 @@ class PriorityQueue(object):
         """
         create a meshgrid of dimension equal to block_sizes (number of parameters specifying task)
         for each tuple combination of parameters initialise key in queue dictionary.
+
+        :return parameter_grid: a numpy array of dimension equal to number of parameters specifying task. 
+                                initialised to a vlue specified in init
         """
-        raise NotImplementedError
+        pranges = []
+        for i in range(len(self.param_ranges)):
+            pranges.append(int((self.param_ranges[i][1] - self.param_ranges[i][0]) / self.block_sizes[i]))
+
+        if self.initial_value:
+            parameter_grid = self.initial_value * np.zeros(tuple(pranges))
+        else:
+            parameter_grid = np.abs(np.random.normal(0, 1, tuple(pranges)))
+        return parameter_grid
   
     # for checking if the queue is empty 
     def isEmpty(self):
         return len(self.queue) == 0
   
-    # for inserting an element in the queue 
-    def insert(self, key, data): 
-        self.queue[key] = data
+    # for inserting an element in the queue
+    def insert(self, key, data):
+        # in case of queue being a dictionary, 'key' is a key into dict object
+        if type(self.queue) == dict:
+            self.queue[key] = data.cpu().detach().numpy()
+        # in case of queue being a np array, 'key' is a list specifying indices
+        elif type(self.queue) == np.ndarray:
+            self.queue[tuple(key)] = data.cpu().detach().numpy()
   
     # for popping an element based on priority heuristic
     def query(self):
@@ -38,18 +64,44 @@ class PriorityQueue(object):
 
         :return value: value from priority queue
         """
-        if self.sample_type == 'max':
-            raise NotImplementedError("Currently not supported - need a way to fill buffer before this would make sense to use")
-            return max(self.queue.items(), key=operator.itemgetter(1))[0]
-        elif self.sample_type == 'epislon_greedy'
-            if random.random() < self.epsilon: # select randomly
-                return random.choice(list(self.queue.keys()))
-            else: # select greedily
-                return max(self.queue.items(), key=operator.itemgetter(1))[0]
-        elif self.sample_type == 'sample_interpolation':
-            raise NotImplementedError("Currently not supported")
-        else:
-            raise ValueError("No sample_type named {}. Please try either 'max', 'epsilon_greedy' or 'sample_interpolation'".format(self.sample_type))
+        def get_max_indices():
+            if type(self.queue) == dict:
+                if self.sample_type == 'max':
+                    raise NotImplementedError("Currently not supported - need a way to fill buffer before this would make sense to use")
+                    return max(self.queue.items(), key=operator.itemgetter(1))[0]
+                elif self.sample_type == 'epsilon_greedy':
+                    if random.random() < self.epsilon: # select randomly
+                        return random.choice(list(self.queue.keys()))
+                    else: # select greedily
+                        return max(self.queue.items(), key=operator.itemgetter(1))[0]
+                elif self.sample_type == 'sample_interpolation':
+                    raise NotImplementedError("Currently not supported")
+                else:
+                    raise ValueError("No sample_type named {}. Please try either 'max', 'epsilon_greedy' or 'sample_interpolation'".format(self.sample_type))
+            elif type(self.queue) == np.ndarray:
+                if self.sample_type == 'max':
+                    raise NotImplementedError("Currently not supported - need a way to fill buffer before this would make sense to use")
+                elif self.sample_type == 'epsilon_greedy':
+                    if random.random() < self.epsilon: # select randomly
+                        random_indices = [np.random.randint(d) for d in self.queue.shape] 
+                        return random_indices
+                    else: # select greedily
+                        max_indices = np.where(self.queue == np.amax(self.queue))
+                        return list(np.concatenate([list(i) for i in max_indices]))
+                elif self.sample_type == 'sample_interpolation':
+                    raise NotImplementedError("Currently not supported")
+                else:
+                    raise ValueError("No sample_type named {}. Please try either 'max', 'epsilon_greedy' or 'sample_interpolation'".format(self.sample_type))
+
+        max_indices = get_max_indices()
+        
+        parameter_values = [i + b * random.random() for (i, b) in zip(max_indices, self.block_sizes)]
+
+        # anneal epsilon
+        if self.epsilon > self.epsilon_final:
+            self.epsilon -= self.epsilon_decay_rate
+
+        return max_indices, parameter_values
 
     def visualise_priority_queue(self):
         """
@@ -57,7 +109,16 @@ class PriorityQueue(object):
 
         Discrete vs continuous, 2d heatmap vs 3d.
         """
-        raise NotImplementedError
+        if type(self.queue) == np.ndarray:
+            if len(self.queue.shape) == 2:
+                fig = plt.figure()
+                plt.imshow(self.queue)
+                plt.colorbar()
+                return fig
+            else:
+                raise ValueError("Visualisation with parameter space dimension > 2 not supported")
+        else:
+            raise NotImplementedError("Visualisation for dictionary queue not implemented")
 
 
     def interpolate_discrete_queue(self):
