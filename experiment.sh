8267949fd216e9df44719c137a4138dc4a0f9fcd
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2019-08-09 10:09:19 -0700

    bash script to run different experiments from tmux shells

---
+++ experiment.sh
@@ -0,0 +1,22 @@
+# EXPERIMENT RUN INSTRUCTIONS
+
+# OPEN TMUX Sessions for experiment and tensorboard
+
+# start tmux sessions one for each experiment
+tmux new -s standard_maml_exp -d
+tmux new -s pq_maml_exp -d
+
+# initialise virtual environments
+tmux send-keys -t "standard_maml_exp" "source /Users/sebastianlee/Dropbox/Documents/Work/Hack/Environments/meta/bin/activate" C-m
+tmux send-keys -t "pq_maml_exp" "source /Users/sebastianlee/Dropbox/Documents/Work/Hack/Environments/meta/bin/activate" C-m
+
+# send keys to run experiments
+tmux send-keys -t "standard_maml_exp" "python main.py maml_config.yaml" C-m
+tmux send-keys -t "pq_maml_exp" "python main.py pq_maml_config.yaml" C-m
+
+# start tmux session for tensorboard, launch tensorboard
+tmux new -s tensorboard -d
+tmux send-keys -t "tensorboard" "tensorboard --logdir /Users/sebastianlee/Dropbox/Documents/Work/Hack/MAML_project/maml_seb/runs/None/" C-m
+
+
+
