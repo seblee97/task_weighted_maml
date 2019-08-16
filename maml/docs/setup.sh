abb80da3511ba9f823adc7152640820b599501b7
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2019-08-16 15:15:56 -0700

    removed previous folder structure with subfolders of maml modules

--- maml/docs/setup.sh
+++
@@ -1,8 +0,0 @@
-# create new python3 virtual environment
-python3 -m venv maml_env
-
-# start virtual environment
-source maml_env/bin/activate
-
-# install python requirements
-pip install -r requirements.txt
