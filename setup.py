b03125b63bbb203ef202964398a9c934670f980c
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2019-08-16 15:16:14 -0700

    minor: changed name of package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="maml-seblee97",
+    name="maml-pq",
     version="0.0.1",
     author="Sebastian Lee",
     author_email="sebalexlee@gmail.com",
