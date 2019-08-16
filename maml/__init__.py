30cc42b3f0d6f14107528b54bcb8ae2dac64b16a
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2019-08-16 15:16:23 -0700

    correct imports in .__init__.py file

--- maml/__init__.py
+++ maml/__init__.py
@@ -1,1 +1,3 @@
-name = "maml"
+from .sinusoid import SineMAML, SinePriorityQueue, SinusoidalNetwork
+from .image_classification import ImageClassificationNetwork
+from .quadratic import QuadraticMAML, QuadraticNetwork
