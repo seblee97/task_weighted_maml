5912b1987fcc8ef54dff8e3a60013dd9fc48252a
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2019-08-19 17:07:26 -0700

    added sample under pdf to experiment suite

--- experiments/experiment.sh
+++ experiments/experiment.sh
@@ -1,24 +1,25 @@
 # kill sessions already running
-tmux kill-session -t pq_maml_exp
+tmux kill-session -t pq_epsilon_maml_exp
 tmux kill-session -t standard_maml_exp
+tmux kill-session -t pq_sample_maml_exp
 tmux kill-session -t tensorboard
 
 # start tmux sessions one for each experiment
 tmux new -s standard_maml_exp -d
-tmux new -s pq_maml_exp -d
+tmux new -s pq_epsilon_maml_exp -d
+tmux new -s pq_sample_maml_exp -d
 
 # initialise virtual environments
 tmux send-keys -t "standard_maml_exp" "source /Users/sebastianlee/Dropbox/Documents/Work/Hack/Environments/meta/bin/activate" C-m
-tmux send-keys -t "pq_maml_exp" "source /Users/sebastianlee/Dropbox/Documents/Work/Hack/Environments/meta/bin/activate" C-m
+tmux send-keys -t "pq_epsilon_maml_exp" "source /Users/sebastianlee/Dropbox/Documents/Work/Hack/Environments/meta/bin/activate" C-m
+tmux send-keys -t "pq_sample_maml_exp" "source /Users/sebastianlee/Dropbox/Documents/Work/Hack/Environments/meta/bin/activate" C-m
 
 # send keys to run experiments
 tmux send-keys -t "standard_maml_exp" "python main.py configs/maml_config.yaml" C-m
-tmux send-keys -t "pq_maml_exp" "python main.py configs/pq_maml_config.yaml" C-m
+tmux send-keys -t "pq_epsilon_maml_exp" "python main.py configs/pq_maml_config.yaml" C-m
+tmux send-keys -t "pq_sample_maml_exp" "python main.py configs/pq_sample_maml_config.yaml" C-m
 
 # start tmux session for tensorboard, launch tensorboard
 tmux new -s tensorboard -d
 tmux send-keys -t "tensorboard" "source /Users/sebastianlee/Dropbox/Documents/Work/Hack/Environments/meta/bin/activate" C-m
 tmux send-keys -t "tensorboard" "tensorboard --logdir results/" C-m
-
-
-
