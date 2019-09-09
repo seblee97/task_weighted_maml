76c7d5a9a80fcc00f7c2c81fde335c70df1eb168
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2019-09-09 12:13:41 +0100

    script to kill all running experiments and tmux shells

---
+++ experiments/kill_experiments.sh
@@ -0,0 +1,6 @@
+# kill sessions already running
+tmux kill-session -t pq_epsilon_maml_exp
+tmux kill-session -t standard_maml_exp
+tmux kill-session -t pq_sample_maml_exp
+tmux kill-session -t pq_importance_maml_exp
+tmux kill-session -t tensorboard
