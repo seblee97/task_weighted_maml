112ed14f0553da7c7d0a4595e89363f64765e2b2
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2019-09-09 12:14:07 +0100

    first commit of script containing custom utils functions, first function is a sampling from n-dimensional array

---
+++ utils/custom_functions.py
@@ -0,0 +1,29 @@
+import numpy as np 
+
+from typing import List
+
+def sample_nd_array(nd_array: np.array) -> [List[int], float]:
+    """
+    For a given n-dimensional array, this method returns an index of
+    the array with probability in proportion to it's value.
+
+    param nd_array: n-dimensional numpy array
+
+    return indices: the index of the array
+    return probability: the probability that this index was chosen
+    """
+
+    normalised_probabilities = nd_array / np.sum(nd_array)
+    normalised_flattened_probabilities = normalised_probabilities.flatten()
+
+    # import pdb; pdb.set_trace()
+    sample_probability = np.random.choice(normalised_flattened_probabilities, p=normalised_flattened_probabilities)
+
+    indices = np.where(normalised_probabilities == sample_probability)
+
+    if len(indices) > 1:
+        # case of duplicate entries
+        random_choice = np.random.randint(len(indices[0]))
+        indices = [int(i[random_choice]) for i in indices]
+
+    return indices, sample_probability
