706a5f3fbba3d151abdf86154efef3d3bf1b7dab
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2019-10-09 18:32:13 +0100

    bash script for image experiments

---
+++ experiments/image_experiment.sh
@@ -0,0 +1,29 @@
+# kill sessions already running
+tmux kill-session -t pq_epsilon_maml_exp
+tmux kill-session -t standard_maml_exp
+tmux kill-session -t pq_sample_maml_exp
+tmux kill-session -t pq_importance_maml_exp
+tmux kill-session -t tensorboard
+
+# start tmux sessions one for each experiment
+tmux new -s standard_maml_exp -d
+tmux new -s pq_epsilon_maml_exp -d
+tmux new -s pq_sample_maml_exp -d
+tmux new -s pq_importance_maml_exp -d
+
+# initialise virtual environments
+tmux send-keys -t "standard_maml_exp" "source ~/envs/meta/bin/activate" C-m
+tmux send-keys -t "pq_epsilon_maml_exp" "source ~/envs/meta/bin/activate" C-m
+tmux send-keys -t "pq_sample_maml_exp" "source ~/envs/meta/bin/activate" C-m
+tmux send-keys -t "pq_importance_maml_exp" "source ~/envs/meta/bin/activate" C-m
+
+# send keys to run experiments
+tmux send-keys -t "standard_maml_exp" "python main.py -base_config image_configs/base_config.yaml -config image_configs/maml_config.yaml" C-m
+tmux send-keys -t "pq_epsilon_maml_exp" "python main.py -base_config image_configs/base_config.yaml -config image_configs/pq_maml_config.yaml" C-m
+tmux send-keys -t "pq_sample_maml_exp" "python main.py -base_config image_configs/base_config.yaml -config image_configs/pq_sample_maml_config.yaml" C-m
+tmux send-keys -t "pq_importance_maml_exp" "python main.py -base_config image_configs/base_config.yaml -config image_configs/pq_importance_maml_config.yaml" C-m
+
+# start tmux session for tensorboard, launch tensorboard
+tmux new -s tensorboard -d
+tmux send-keys -t "tensorboard" "source ~/envs/meta/bin/activate" C-m
+tmux send-keys -t "tensorboard" "tensorboard --logdir results/" C-m
