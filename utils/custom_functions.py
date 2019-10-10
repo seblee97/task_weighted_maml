5b4a3f9ade109a659b108d7af5188870892e45ec
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2019-10-10 10:25:18 +0100

    made case for 1d param space

--- utils/custom_functions.py
+++ utils/custom_functions.py
@@ -12,18 +12,22 @@ def sample_nd_array(nd_array: np.array) -> [List[int], float]:
     return indices: the index of the array
     return probability: the probability that this index was chosen
     """
-
     normalised_probabilities = nd_array / np.sum(nd_array)
     normalised_flattened_probabilities = normalised_probabilities.flatten()
 
-    # import pdb; pdb.set_trace()
     sample_probability = np.random.choice(normalised_flattened_probabilities, p=normalised_flattened_probabilities)
 
-    indices = np.where(normalised_probabilities == sample_probability)
-
-    if len(indices) > 1:
-        # case of duplicate entries
-        random_choice = np.random.randint(len(indices[0]))
-        indices = [int(i[random_choice]) for i in indices]
+    if len(nd_array.shape) == 1:
+        indices = np.where(normalised_probabilities == sample_probability)[0]
+        if len(indices) > 1:
+            # case of duplicate entries
+            random_choice = np.random.randint(len(indices))
+            indices = [indices[random_choice]]
+    else:
+        indices = np.where(normalised_probabilities == sample_probability)
+        if len(indices) > 1:
+            # case of duplicate entries
+            random_choice = np.random.randint(len(indices[0]))
+            indices = [int(i[random_choice]) for i in indices]
 
     return indices, sample_probability
