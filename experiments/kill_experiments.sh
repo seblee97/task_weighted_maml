1b766ad73a0956ee91770fe804c42e6fc4336e19
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2019-10-14 11:54:11 +0100

    added sample delta to kill call

--- experiments/kill_experiments.sh
+++ experiments/kill_experiments.sh
@@ -3,4 +3,5 @@ tmux kill-session -t pq_epsilon_maml_exp
 tmux kill-session -t standard_maml_exp
 tmux kill-session -t pq_sample_maml_exp
 tmux kill-session -t pq_importance_maml_exp
+tmux kill-session -t pq_sample_delta
 tmux kill-session -t tensorboard
